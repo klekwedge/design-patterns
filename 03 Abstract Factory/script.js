d54d40b98b038349af698913c72607c1dbe28f46
klekkwedge <antonov.andrey.10101@gmail.com>
2022-09-28 10:03:28 +0300

    feat: add 3 lesson

---
+++ 03 Abstract Factory/script.js
@@ -0,0 +1,31 @@
+// Generating (Порождающий)
+
+function bmwProducer(kind) {
+  return kind === 'sport' ? sportCarFactory : familiCarFactory;
+}
+
+function sportCarFactory() {
+  return new Z4();
+}
+
+function familiCarFactory() {
+  return new I3();
+}
+
+class Z4 {
+  info() {
+    return 'Z4 is a Sport car!';
+  }
+}
+
+class I3 {
+  info() {
+    return 'I4 is a Family car!';
+  }
+}
+
+const produce = bmwProducer('sport');
+
+const myCar = new produce();
+
+console.log(myCar.info());
