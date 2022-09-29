3d59b21540da881e17e5847e873c4d94bcdeefb9
klekkwedge <antonov.andrey.10101@gmail.com>
2022-09-29 21:33:06 +0300

    feat: add 9 lesson

---
+++ 2. Structural/09 Adapter/script.js
@@ -0,0 +1,44 @@
+class Engine2 {
+  simpleInterface() {
+    console.log('Engine 2.0');
+  }
+}
+
+class EngineV8 {
+  complecatedInterface() {
+    console.log('Engine V8!');
+  }
+}
+
+class EnigneV8Adapter {
+  constructor(engine) {
+    this.engine = engine;
+  }
+
+  simpleInterface() {
+    this.engine.complecatedInterface();
+  }
+}
+
+class Auto {
+  startEngine(engine) {
+    engine.simpleInterface();
+  }
+}
+
+
+const myCar = new Auto();
+const oldEngine = new Engine2();
+
+myCar.startEngine(oldEngine);
+
+const myCar2 = new Auto();
+const engineAdapter = new EnigneV8Adapter(new EngineV8());
+
+myCar2.startEngine(engineAdapter);
+
+// Error
+// const myCar3 = new Auto();
+// const engineAdapter2 = new EngineV8();
+
+// myCar3.startEngine(engineAdapter2);
