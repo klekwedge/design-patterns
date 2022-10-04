ac279770af268b3464d7e35318ff6497e85c492a
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-04 10:52:53 +0300

    feat: add 22 lesson

---
+++ 3. Behavioral/22 State/script.js
@@ -0,0 +1,56 @@
+class OrderStatus {
+  constructor(name, nextStatus) {
+    this.name = name;
+    this.nextStatus = nextStatus;
+  }
+
+  next() {
+    return new this.nextStatus();
+  }
+}
+
+class WaitingForPayment extends OrderStatus {
+  constructor() {
+    super('waitingForPayment', Shipping);
+  }
+}
+
+class Shipping extends OrderStatus {
+  constructor() {
+    super('shipping', Delivered);
+  }
+}
+
+class Delivered extends OrderStatus {
+  constructor() {
+    super('delivered', Delivered);
+  }
+}
+
+class Order {
+  constructor() {
+    this.state = new WaitingForPayment();
+  }
+
+  nextState() {
+    this.state = this.state.next();
+  }
+
+  cancelOrder() {
+    this.state.name === 'waitingForPayment'
+      ? console.log('Order is canceled')
+      : console.log('Order can not be canceled');
+  }
+}
+
+const myOrder = new Order();
+
+console.log(myOrder.state.name); // waitingForPayment
+myOrder.cancelOrder(); // Order is canceled
+
+myOrder.nextState();
+console.log(myOrder.state.name); // shipping
+myOrder.cancelOrder(); // Order can not be canceled
+
+myOrder.nextState();
+console.log(myOrder.state.name); // delivered
